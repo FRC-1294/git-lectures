1f52a168eaf78e1f3690e2a0338cd1980c384a8d
Thanh Doan <tdoan@ieee.org>
2023-06-20 01:12:47 -0700

    Refactor minimax function

--- assignments/assign1/ai.js
+++ assignments/assign1/ai.js
@@ -1,55 +1,54 @@
-function aiMove() {
-  let bestScore = -Infinity;  
-  let move;
-  
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {      
-      if (board[i][j] == '') {        
-        board[i][j] = ai;
-        let score = minimax(board, false);
-        board[i][j] = '';
-        if (score > bestScore) {
-          bestScore = score;  
-          move = {i, j};
-        }        
-      }
-    }
-  }
-  
+const scores = { X: 1, O: -1, tie: 0 };
+function aiMove() {  
+  let move = maxScore(board).move;
   board[move.i][move.j] = ai;
   player = human;
 }
 
-const scores = { X: 1, O: -1, tie: 0 };
-function minimax(board, scoreMaximizer) {
-  const result = findWinner();
-  if (result != null) return scores[result];  
-    
-  if (scoreMaximizer) {
-    let bestScore = -Infinity;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == '') { 
-          board[i][j] = ai;
-          let score = minimax(board, false);
-          board[i][j] = '';
-          bestScore = min(score, bestScore);
-        }
-      }
-    }
-    return bestScore;
-  } else {
-    let bestScore = Infinity;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == '') { 
-          board[i][j] = human;
-          let score = minimax(board, true);
-          board[i][j] = '';
-          bestScore = max(score, bestScore);
-        }
-      }
-    }
-    return bestScore;
+function maxScore(board) {
+  let bestScore = -Infinity, move = null;
+  const winner = findWinner();
+  if (winner != null) {
+    bestScore = scores[winner];
+    return {bestScore, move};
   }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {      
+      if (board[i][j] == '') {        
+        board[i][j] = ai;
+        let score = minScore(board).bestScore;
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = {i, j};
+        }
+      }
+    }
+  }
+  return {bestScore, move};
+}
+
+function minScore(board) {
+  let bestScore = Infinity, move = null;
+  const winner = findWinner();
+  if (winner != null) {
+    bestScore = scores[winner];
+    return {bestScore, move};
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {      
+      if (board[i][j] == '') {        
+        board[i][j] = human;
+        let score = maxScore(board).bestScore;
+        board[i][j] = '';
+        if (score < bestScore) {
+          bestScore = score;
+          move = {i, j};
+        }       
+      }
+    }
+  }
+  return {bestScore, move};
 }
